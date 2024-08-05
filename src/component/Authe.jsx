20b97cdecd68ac724aaa2230287150a93af54584
Onyekwere Michael <onyekweremichael55@gmail.com>
2024-08-05 15:31:32 +0100

    chat

--- src/component/Authe.jsx
+++ src/component/Authe.jsx
@@ -1,27 +1,30 @@
-import Cookies from "universal-cookie"
-import { auth, provider } from "../Firebase/firebase.config"
-import { signInWithPopup } from "firebase/auth"
+import React from "react";
+import Cookies from "universal-cookie";
+import { auth, provider } from "../Firebase/firebase.config";
+import { signInWithPopup } from "firebase/auth";
 
-const cookies = new Cookies()
+const cookies = new Cookies();
 
-function Authe({setAuth}) {
-  const signinwithGoggle = async () => {
+function Authe({ setAuth }) {
+  const signinwithGoogle = async () => {
     try {
-      const result = await signInWithPopup(auth, provider)
-      cookies.set('auth-token', result.user.refreshToken)
+      const result = await signInWithPopup(auth, provider);
+      cookies.set('auth-token', result.user.refreshToken);
       console.log(result);
-      setAuth(true)
+      setAuth(true);
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   return (
     <div className="sign-in">
       <p>Sign in with Google to continue</p>
-      <button onClick={signinwithGoggle} className="sign-in-Button">Sign in with Google run</button>
+      <button onClick={signinwithGoogle} className="sign-in-button">
+        Sign in with Google dev
+      </button>
     </div>
-  )
+  );
 }
 
-export default Authe
+export default Authe;
